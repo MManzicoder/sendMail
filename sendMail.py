16857d02c2c38b6e68db39da84dd2d709cca273e
Mmanzi monnierey <mudahemukamanzi@yahoo.com>
2021-11-28 15:23:02 +0200

    fn

--- sendMail.py
+++ sendMail.py
@@ -1,16 +1,18 @@
 import smtplib;
 import os;
-userName = os.environ.get("EMAIL_User_Name")
+EMAIL_ADRESS_CONFIG = os.environ.get("EMAIL_User_Name")
 password = os.environ.get("Email_Pass")
 with smtplib.SMTP("smtp.gmail.com", 587) as smtp:
     smtp.ehlo()
     smtp.starttls()
     smtp.ehlo()
 
-    smtp.login(userName, password)
+    smtp.login(EMAIL_ADRESS_CONFIG, password)
 
-    subject = "Email send testing!!!"
-    body = "Hi this is mail transferring with Python"
+    subject = "Networking SMTP Assignment"
+    body = """Hello sir this is mail transferring with Python using smtplib. 
+    \nHere is the link of codes on github https://github.com/MManzicoder/sendMail \n 
+    Have a good day!."""
     msg = f'Subject: {subject}\n\n{body}'
 
-    smtp.sendmail(userName, "mmanzicder@gmail.com", msg)
+    smtp.sendmail(EMAIL_ADRESS_CONFIG, "gbaziramwabo@gmail.com", msg)
